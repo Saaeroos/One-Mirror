13542e13c5574883bf58b37cc7e31ae7a3534d9e
Mohamed Alhayek <basel333999@MacBook-Pro-van-basel.local>
2018-03-03 19:00:50 +0100

    added front end for admin

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 import AdminLog from './AdminLog';
+import AdminStReg from './AdminStReg';
 
 class App extends Component {
     render() {
         return (
             <Router>
                 <Switch>
-                    <Route path='/admin' component={AdminLog} />
+                    <Route exact path='/admin' component={AdminLog} />
+                    <Route path='/admin/student/register' component={AdminStReg} />
                     <Route render={function(){
                         return <p> Not Found</p>
                     }} />
