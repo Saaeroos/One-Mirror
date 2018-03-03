fb5dbfee7b4646a7dadc50b8b3bf7b2bc15a96fc
srinidhi <srinidhivk1989@gmail.com>
2018-03-03 19:01:35 +0100

    added addscore and admindashboard components

---
+++ frontend/src/components/AdminDashboard.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+class AdminDashboard extends Component {
+  handleAddscore()
+  {
+    window.location.href='/admin/addscore';
+  }
+render(){
+  return (
+  <div>
+    <h1>Admin Dashboard</h1>
+    <nav className="nav">
+        <a className="nav-link disabled" href="#">List Of Students</a>
+        <a className="nav-link active"  href="#">Registration</a>
+      </nav>
+    <div className="table-responsive-md">
+    <table className="table table-hover">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col" colSpan={3}>Name</th>
+            <th scope="col" colSpan={3}>Actions</th>
+
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">12453</th>
+            <td colSpan={3}>Jac Thon</td>
+            <td><button type="button"  className="btn btn-primary" >Edit</button></td>
+            <td><button type="button"  className="btn btn-info" >view Profile</button></td>
+            <td><button type="button" className="btn btn-success" onClick={this.handleAddscore}>Add Score</button></td>
+
+          </tr>
+          <tr>
+            <th scope="row">22354</th>
+            <td colSpan={3}>Jacob Thornton</td>
+            <td><button type="button" className="btn btn-primary">Edit</button></td>
+            <td><button type="button" className="btn btn-info">view Profile</button></td>
+            <td><button type="button" className="btn btn-success" onClick={this.handleAddscore}>Add Score</button></td>
+          </tr>
+          <tr>
+            <th scope="row">32653</th>
+            <td colSpan={3}>Larry the Bird</td>
+            <td><button type="button" className="btn btn-primary">Edit</button></td>
+            <td><button type="button" className="btn btn-info">view Profile</button></td>
+            <td><button type="button" className="btn btn-success" onClick={this.handleAddscore}>Add Score</button></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+  )
+
+}
+}
+export default AdminDashboard;
--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -2,13 +2,18 @@ import React, { Component } from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 import AdminLog from './AdminLog';
+import AdminDashboard from './AdminDashboard';
+import AddScore from './AddScore';
+
 
 class App extends Component {
     render() {
         return (
             <Router>
                 <Switch>
-                    <Route path='/admin' component={AdminLog} />
+                    <Route exact path='/admin' component={AdminLog} />
+                    <Route path='/admin/dashboard' component={AdminDashboard}/>
+                    <Route path='/admin/addscore' component={AddScore}/>
                     <Route render={function(){
                         return <p> Not Found</p>
                     }} />
