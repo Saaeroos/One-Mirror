e05211e91ec15fdbd7577abf97813c601ce30627
anthony <anthony_wever@yahoo.com>
2018-03-03 16:09:13 +0100

    creating and adding student login page

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 import AdminLog from './AdminLog';
+import StudentLogin from './StudentLogin';
 
 class App extends Component {
     render() {
         return (
             <Router>
                 <Switch>
+                    <Route path='/studentlogin' component={StudentLogin} />
                     <Route path='/admin' component={AdminLog} />
                     <Route render={function(){
                         return <p> Not Found</p>
