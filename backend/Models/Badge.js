381e0db34551c61a4a5e8ac77d75ab2552e0ba5b
srinidhi <srinidhivk1989@gmail.com>
2018-03-07 18:16:48 +0100

    fix badge model

--- backend/Models/Badge.js
+++ backend/Models/Badge.js
@@ -3,36 +3,6 @@ var Schema = mongoose.Schema;
 
 var BadgeSchema = new Schema({
 
-<<<<<<< HEAD
-    StudentID:{
-        type: String,
-        required: true
-    },
-    Badge1Status:{
-        type: Number,
-        Default: 0
-    },
-    Badge2Status:{
-        type: Number,
-        Default: 0
-    },
-    Badge3Status:{
-        type: Number,
-        Default: 0
-    },
-    Badge4Status:{
-        type: Number,
-        Default: 0
-    },
-    Badge5Status:{
-        type: Number,
-        Default: 0
-    },
-    Badge6Status:{
-        type: Number,
-        Default: 0
-    }
-=======
    StudentID:{
        type: String,
        required: true
@@ -61,7 +31,6 @@ var BadgeSchema = new Schema({
        type: Number,
        Default: 0
    }
->>>>>>> srinidhi4
 })
 
 module.exports = mongoose.model('Badge', BadgeSchema);
